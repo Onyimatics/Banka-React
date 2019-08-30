d94a0cf0b2d4e032166a500c7f514b496a8025cc
Onyinye Ezike <onyinyeezike@Onyinyes-MacBook-Pro.local>
2019-08-30 20:13:43 +0100

    mix

--- webpack-build-utils/webpack.production.js
+++ webpack-build-utils/webpack.production.js
@@ -1,25 +1,39 @@
-const path = require('path')
-
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractWebpackPlugin = require('mini-css-extract-plugin');
+const glob = require('glob');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const PATHS = {
+  src: path.join(__dirname, 'src')
+};
 module.exports = {
-  devtool: "source-map",
-      output: {
-    path: path.resolve(__dirname, "./src/dist"),
-      filename: "app.js",
-        publicPath: "/"
+  mode: 'production',
+  output: {
+    filename: '[name].[contentHash].bundle.js',
+    path: path.resolve(__dirname, '../dist')
   },
-  devServer: {
-    historyApiFallback: true,
+  plugins: [
+    new MiniCssExtractWebpackPlugin({ filename: '[name].[contentHash].css' }),
+    new PurgecssPlugin({
+      paths: glob.sync(${ PATHS.src }/**/ *, { nodir: true })
+    }),
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true
+        }
+      })
+    ]
   }
 };
-
-// {
-//   mode: "production",
-//     devtool: "source-map",
-//       output: {
-//     path: path.resolve(__dirname, "./dist"),
-//       filename: "app.js",
-//         publicPath: "/"
-//   },
-//   optimization: {
-//     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
-//   }
